df9b76139aabd34ef5a02d102050b2ee05f8ff15
castykawira <murithicasty@gmail.com>
2023-04-24 15:01:24 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,28 +6,16 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int s;
 
-	for (a = 0; a <= 9; a++)
+	for (s = 48; s < 58; s++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		putchar(s);
+		if (s != 57)
 		{
-			for (c = b + 1; c <= 9; c++)
-			{
-				for (d = c + 1; d <= 9; d++)
-				{
-					putchar(a | 48);
-					putchar(',');
-					putchar(b | 48);
-					putchar(',');
-					putchar(c | 48);
-					putchar(',');
-					putchar(d | 48);
-					putchar('\n');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
-	}
-
+	putchar('\n');
 	return (0);
 }
