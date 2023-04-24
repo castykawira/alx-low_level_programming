7cc1b798d49a3214e422df54285ed20144488e0c
castykawira <murithicasty@gmail.com>
2023-04-24 14:17:41 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				for (d = c + 1; d <= 9; d++)
+				{
+					putchar(a | 48);
+					putchar(',');
+					putchar(',');
+					putchar(b | 48);
+					putchar(',');
+					putchar(',');
+					putchar(c | 48);
+					putchar(',');
+					putchar(',');
+					putchar(d | 48);
+					putchar('\n');
+				}
+			}
+		}
+
+		return (0);
+	}
