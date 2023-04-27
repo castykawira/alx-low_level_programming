1bfc3e0fd6da544268dfc9d3b869a6745e1e9839
castykawira <murithicasty@gmail.com>
2023-04-27 07:23:27 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,15 @@ void print_line(int n)
 	{
 		_putchar('\n');
 	}
-	else if (n > 0)
+	else
 	{
 		int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
 }
 
