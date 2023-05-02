314a139b9868447d0bfaf8651afa0b89791b21cb
castykawira <murithicasty@gmail.com>
2023-05-02 12:18:53 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,17 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
