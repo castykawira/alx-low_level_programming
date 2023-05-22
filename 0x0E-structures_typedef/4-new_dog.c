67136a4e76af570eebd2a5a0ff838aeb0dd9ae42
castykawira <murithicasty@gmail.com>
2023-05-22 09:39:36 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,8 +27,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->name, name);
-	strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
