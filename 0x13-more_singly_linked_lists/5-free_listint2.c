86b54a6f69704da966d62239ab7411d09bd2aba1
castykawira <murithicasty@gmail.com>
2023-06-06 05:47:42 -0700

    listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
-	listint_t *next;
 
-	while (current)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		next = current->next;
-		 free(current);
-		 current = next;
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
 
 	*head = NULL;
