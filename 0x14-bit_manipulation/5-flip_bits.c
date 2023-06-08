9e582ca642daaf710be67768ea54b44c3ebf1918
castykawira <murithicasty@gmail.com>
2023-06-08 16:22:23 -0700

    100

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,17 +5,17 @@
  * to transform one number into another
  * @n: The first unsigned long integer
  * @m: The second unsigned long integer
- * Return: The number of bits that need to be flipped
+ * Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int x = n ^ m;
 	unsigned int count = 0;
 
-	while (x)
+	for (int i = 0; i < 64; i++)
 	{
-		 count += x & 1;
-		 x >>= 1;
+		 if (x & (1 << i))
+			 count++;
 	}
 	return (count);
 }
