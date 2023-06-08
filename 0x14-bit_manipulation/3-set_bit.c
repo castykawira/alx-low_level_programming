c4fcf55141c2bad782409e6fec0d22ae28cc74ec
castykawira <murithicasty@gmail.com>
2023-06-08 14:29:13 -0700

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,11 +7,11 @@
  * Return: The value of the bit at the specified index,
  * or -1 if an error occurred
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= 64)
 		return (-1);
 
-	*n = ((1UL << index) | *n);
+	*n |= (1 << index);
 	return (1);
 }
