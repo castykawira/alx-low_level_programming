15f357b6b8acbc67f2df887f69ed26446152629e
castykawira <murithicasty@gmail.com>
2023-06-12 11:30:33 -0700

    1 task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,17 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 int ret;
+text_length = 0;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 
 if (text_content != NULL)
-ret = write(fd, text_content, strlen(text_content));
+ret = write(fd, text_content, text_length);
 
 else
 ret = write(fd, "", 0);
