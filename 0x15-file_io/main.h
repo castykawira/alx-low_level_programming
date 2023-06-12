d51205b381a6b58d3874ceba4d28ef282a6d7b71
castykawira <murithicasty@gmail.com>
2023-06-12 08:27:30 -0700

    0 task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
