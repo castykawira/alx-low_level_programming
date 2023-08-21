863ca6d98025ed8deecc7c130345d13c8bc62ddf
castykawira <murithicasty@gmail.com>
2023-08-21 15:05:02 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,48 @@
+/**
+ * add - Adds two integers and returns the result.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * subtract - Subtracts the second integer from the first
+ * and returns the result
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The result of subtracting b from a.
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * multiply - Multiplies two integers and returns the result.
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The product of a and b.
+ */
+int multiply(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divide - Divides the first integer by the second and returns the result.
+ * @a: The numerator.
+ * @b: The denominator (must be non-zero).
+ * Return: The result of dividing a by b.
+ */
+int divide(int a, int b)
+{
+	if (b == 0)
+	{
+		return (-1);
+	}
+	return (a / b);
+}
