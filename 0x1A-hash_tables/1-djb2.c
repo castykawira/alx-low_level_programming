9bf3a9b1adca1e90ccc3f165916013737c3db34f
castykawira <murithicasty@gmail.com>
2023-09-08 10:45:08 -0700

    question 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,15 +7,14 @@
  * Return: The calculated hash value as an unsigned long int
  */
 unsigned long int hash_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int result;
+	int count;
 
 	hash = 5381;
-	while ((result = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+	while ((count = *str++))
+		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
 
 	return (hash);
 }
