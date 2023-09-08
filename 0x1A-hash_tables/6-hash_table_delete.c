b3f7a9cfa1840539c1fb35a682ca130df9caba33
castykawira <murithicasty@gmail.com>
2023-09-08 13:17:53 -0700

    question 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int j;
+	hash_node_t *current, *temp;
+
+	if (!ht)
+		return;
+
+	for (j = 0; j < ht->size; j++)
+	{
+		current = ht->array[j];
+		while (current)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
